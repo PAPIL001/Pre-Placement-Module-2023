6202db2c198bbc1790f3b637e72c0cbecab23852
PAPIL001 <55889535+PAPIL001@users.noreply.github.com>
2022-07-16 15:23:18 +0530

    Create permutation.java

---
+++ ALGO I/Day 6/permutation.java
@@ -0,0 +1,34 @@
+class Solution {
+    
+    public boolean checkInclusion(String s1, String s2) {
+        
+        if (s1.length() > s2.length()) {
+            
+            return false;
+        }
+        
+        
+        int[] freqS1 = new int[26];
+        int[] freqS2 = new int[26];
+        
+        for (int i=0; i<s1.length(); i++) {
+            
+            freqS1[s1.charAt(i) - 'a']++;
+            freqS2[s2.charAt(i) - 'a']++;
+        }
+        
+        for (int i=s1.length(); i<s2.length(); i++) {
+            
+            if (Arrays.equals(freqS1, freqS2)) {
+                
+                return true;
+            }
+            
+           
+            freqS2[s2.charAt(i - s1.length()) - 'a']--;
+            freqS2[s2.charAt(i) - 'a']++;
+        }
+        
+        return Arrays.equals(freqS1, freqS2);
+    }
+}
